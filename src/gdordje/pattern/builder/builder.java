769921758ae4277f4ed840cdfb0d29fc48c1c914
garab.dordje <108vadim@mail.ru>
2014-10-23 02:02:54 +0400

    start

--- src/gdordje/pattern/builder/builder.java
+++ src/gdordje/pattern/builder/builder.java
@@ -4,6 +4,13 @@ public class builder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	    Director director = new Director();
+	    ComputerBuilder cheapComputerBuilder = new CheapComputerBuilder();
+
+	    director.setComputerBuilder(cheapComputerBuilder);
+	    director.constructComputer();
+
+	    Computer computer = director.getComputer();
 		System.out.println("sex");
 	}
 
