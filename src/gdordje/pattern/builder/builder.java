48f1c3663ed5662cc322808f5c48e27d3156fe44
garab.dordje <108vadim@mail.ru>
2014-10-23 01:56:45 +0400

    my

---
+++ src/gdordje/pattern/builder/builder.java
@@ -0,0 +1,10 @@
+package gdordje.pattern.builder;
+
+public class builder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sex");
+	}
+
+}
---
+++ src/gdordje/pattern/builder/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jedi
+ *
+ */
+package gdordje.pattern.builder;
